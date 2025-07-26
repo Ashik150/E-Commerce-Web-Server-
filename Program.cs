8b47acda242d878a1209d190152271d55de6e2df
Ashik150 <sskhan4409@gmail.com>
2025-07-26 14:13:03 +0600

    Category Crud operation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.Connections;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -13,18 +15,40 @@ if(app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
-var products = new List<Product>
-{
-    new Product("Laptop", 999.99m),
-    new Product("Smartphone", 499.99m),
-    new Product("Tablet", 299.99m)
-};
+List<Category> categories = new List<Category>();
 
 app.MapGet("/", () =>
 {
-    return Results.Ok(products);
+    return "Welcome to E-Commerce Site";
+});
+
+//Read Categories
+app.MapGet("/api/categories", () =>
+{
+    return Results.Ok(categories);
+});
+
+//Post Categories
+app.MapPost("/api/categories", () =>
+{
+    var category = new Category
+    {
+        CategoryId = Guid.NewGuid(),
+        Name = "Electronics",
+        Description = "All electronic items",
+        CreatedAt = DateTime.UtcNow
+    };
+    categories.Add(category);
+    return Results.Created($"/api/categories/{category.CategoryId}",category);
 });
 
 app.Run();
 
-public record Product(string Name, decimal price);
+public record Category
+{
+    public Guid CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+};
