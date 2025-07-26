4dd532f1c5934f2327e921168bdee95a21027b66
Ashik150 <sskhan4409@gmail.com>
2025-07-26 09:59:05 +0600

    Basic Request-Response Structure Done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
 
+var products = new List<Product>
+{
+    new Product("Laptop", 999.99m),
+    new Product("Smartphone", 499.99m),
+    new Product("Tablet", 299.99m)
+};
+
+app.MapGet("/", () =>
+{
+    return Results.Ok(products);
+});
+
 app.Run();
+
+public record Product(string Name, decimal price);
